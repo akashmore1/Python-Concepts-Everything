5c90d9e80710fd5d9de1f64288920fe5db3a1077
Akash More <akashkmore1@gmail.com>
2022-07-14 19:32:43 +0530

    Learn print, input methods and variables

---
+++ Day-1_Working_with_Variables/main.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+
+print("Hello World!\nHello World!\nHello World!")
+
+print("Hello" + " " + "Angela")
+
+print("What is your name?")
+print(len(input("name: ")))
+
+print("What is your first name?")
+firstname = input("First name: ")
+print("What is your last name?")
+lastname = input("Last name: ")
+print("Your full name is " + firstname + " " + lastname)
