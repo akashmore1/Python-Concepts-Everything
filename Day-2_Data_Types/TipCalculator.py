b17dab069b677c15d401a5375a7214168b7e9c3b
Akash More <akashkmore1@gmail.com>
2022-07-15 21:47:33 +0530

    Correct type for tip calculator

--- Day-2_Data_Types/TipCalculator.py
+++ Day-2_Data_Types/TipCalculator.py
@@ -12,7 +12,7 @@ print("Welcome to the tip calculator!")
 total_bill = input("What was the total bill? " )
 tip = input("How much tip would you like to give? 10, 12, or 15? ")
 number_of_people = input("How many people to split the bill? ")
-bill_with_tip = float(total_bill) + float(total_bill) * int(total_bill) / 100
+bill_with_tip = float(total_bill) + float(total_bill) * float(total_bill) / 100
 bill_individual = round(bill_with_tip / int(number_of_people), 2)
 
 print(f"Each person should pay: ${bill_individual}")
