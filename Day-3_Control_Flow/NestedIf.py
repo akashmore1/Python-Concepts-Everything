3f521bd1099c24cbcaee080649cd8a89f9b6d599
Akash More <akashkmore1@gmail.com>
2022-07-16 19:43:24 +0530

    Learn elif statement

--- Day-3_Control_Flow/NestedIf.py
+++ Day-3_Control_Flow/NestedIf.py
@@ -2,9 +2,11 @@ height = int(input("What is your height? "))
 age = int(input("What is your age? "))
 
 if height > 120:
-    if age >= 18:
-        print("You can ride with 12$ bill")
-    else:
+    if age < 12:
+        print("Your bill is 4$")
+    elif age < 18:
         print("You can ride with 7$ bill")
+    else:
+        print("You can ride with 12$ bill")
 else:
     print("You cannot ride!")
