6f64fd827d6bd667f6de390406995241c16651e0
Akash More <akashkmore1@gmail.com>
2022-07-22 15:50:53 +0530

    Generate password in random way

---
+++ Day-5_Python_Loops/password_generator.py
@@ -0,0 +1,57 @@
+# Password Generator Project
+import random
+
+letters = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+    'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+random_letter_arr = []
+random_symbol_arr = []
+random_number_arr = []
+
+for number in range(0, nr_letters):
+    rand_number = random.randint(0, len(letters) - 1)
+    random_letter_arr.append(letters[rand_number])
+
+for number in range(0, nr_symbols):
+    rand_number = random.randint(0, len(symbols) - 1)
+    random_symbol_arr.append(symbols[rand_number])
+
+for number in range(0, nr_numbers):
+    rand_number = random.randint(0, len(numbers) - 1)
+    random_number_arr.append(numbers[rand_number])
+
+join_arr = random_letter_arr + random_symbol_arr + random_number_arr
+
+pass_str = ''.join(join_arr)
+print(pass_str)
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+random_arr = []
+hard_level_arr = []
+counter = 0
+sim_counter = 0
+
+while len(random_arr) < len(join_arr):
+    fin_rand_num = random.randint(0, len(join_arr) - 1)
+    if fin_rand_num not in random_arr:
+        random_arr.append(fin_rand_num)
+
+for number in range(0, len(random_arr)):
+    hard_level_arr.append(join_arr[random_arr[number]])
+
+hard_pass_string = ''.join(hard_level_arr)
+print(hard_pass_string)
