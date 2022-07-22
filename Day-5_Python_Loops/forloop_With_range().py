1958fbbdd6207b6c0989789427261870dd1958dc
Akash More <akashkmore1@gmail.com>
2022-07-22 14:01:20 +0530

    learn range function in for

---
+++ Day-5_Python_Loops/forloop_With_range().py
@@ -0,0 +1,6 @@
+# range() function take two numbers as argument. and return number from start number to end number -1.
+
+for number in range(1, 11):
+    print(number)
+
+# we can give 3rd argument to range() function which will define how long the step will be.
