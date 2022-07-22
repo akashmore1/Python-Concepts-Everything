8c42ca01834ccc636dcb9efd76666e8ec1e44b9e
Akash More <akashkmore1@gmail.com>
2022-07-22 14:10:28 +0530

    Sum of even numbers below 100

--- Day-5_Python_Loops/sumof100numbers.py
+++ Day-5_Python_Loops/sumof100numbers.py
@@ -4,3 +4,11 @@ for number in range(1, 101):
     total += number
 
 print(total)
+
+# Sum of even numbers below 100
+total = 0
+
+for number in range(2, 101, 2):
+    total += number
+
+print(total)
