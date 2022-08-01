c77a12faeeb626a75ebbe65a7f7015b68399d83f
Akash More <akashkmore1@gmail.com>
2022-08-01 23:52:13 +0530

    Complete functions with return statement

--- Day-10_Functions_With_Outputs/functions_output.py
+++ Day-10_Functions_With_Outputs/functions_output.py
@@ -1,5 +1,6 @@
 # return statement in python functions behave similar like js.
 # We can have multiple return statements in python functions, just like js.
+# Statements below return will not be executed just kih=ke js.
 
 def sum_two_numbers(num1, num2):
     return num1 + num2
