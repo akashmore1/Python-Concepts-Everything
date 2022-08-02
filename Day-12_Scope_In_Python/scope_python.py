fa2e19e42e31281b24195e8dffaa072b6d893a54
Akash More <akashkmore1@gmail.com>
2022-08-02 21:35:05 +0530

    Modify global variable in python function

--- Day-12_Scope_In_Python/scope_python.py
+++ Day-12_Scope_In_Python/scope_python.py
@@ -17,4 +17,48 @@ def dummy_call():
 
 
 dummy_call()
-print(f_name)  # =>This is not a valid syntax
+# print(f_name)  # =>This is not a valid syntax
+
+# Modifying vs reading global scope variables.
+
+number = 4
+
+
+def read_number():
+    number = 6
+    print(f"Number inside function is {number}")  # prints 6
+
+
+read_number()
+
+print(f"Number outside function is {number}")  # prints 4
+
+# The above thing is happening because, number inside function is considered as a new variable.
+# python highly recommend not to modify global variables inside a function. Thas's why this is happening.
+# so If we write number += number inside function after removing line 28, it will give us an error.
+
+# So if we have to modify a global variable inside function we use global keyword
+# e.g.
+global_var = 24
+
+
+def change_global_var():
+    global global_var
+    global_var += 4
+
+
+change_global_var()
+# ^ In this way we can use global keyword to modify global variables inside function =>(Never do this though)
+
+# Instead what we can do is return value in function and again assign it to global variable
+# e.g.
+
+var_global = 44
+
+
+def correct_modify_global_var():
+    return var_global + 4
+
+
+var_global = correct_modify_global_var()
+# In this way we should modify global variable
