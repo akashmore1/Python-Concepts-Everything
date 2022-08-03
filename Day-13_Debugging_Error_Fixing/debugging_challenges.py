bc649e6c6421f9d3b7df7ad21f58374c0244418c
Akash More <akashkmore1@gmail.com>
2022-08-03 09:40:36 +0530

    challenge 2

--- Day-13_Debugging_Error_Fixing/debugging_challenges.py
+++ Day-13_Debugging_Error_Fixing/debugging_challenges.py
@@ -4,3 +4,13 @@ if number % 2 == 0:
     print("This is an even number.")
 else:
     print("This is an odd number.")
+
+
+# 2
+def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
