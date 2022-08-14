fe738f09bb3eccefab20792325301afd64c1de97
Akash More <akashkmore1@gmail.com>
2022-08-14 16:19:56 +0530

    Solve debugging challenge

--- Day-13_Debugging_Error_Fixing/debugging_challenges.py
+++ Day-13_Debugging_Error_Fixing/debugging_challenges.py
@@ -22,3 +22,28 @@ my_function()
 dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
 dice_num = randint(0, 5)
 print(dice_imgs[dice_num], dice_num)
+
+# ====================== Leap Year Challenge ===================== #
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
+
+# ====================== Fizz Buzz Challenge ===================== #
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
