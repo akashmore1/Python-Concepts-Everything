e99e80ca95fbe4d7cbcf7704368c98c60cd2e5fd
Akash More <akashkmore1@gmail.com>
2022-08-17 21:45:04 +0530

    Add attribute to object using constructor function

---
+++ Day-17_Project_In_Object_Oriented_Programming/3-Add_Attribute_Using_Constructor.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, uid, name):
+        self.user_id = uid,
+        self.user_name = name
+
+
+user_1 = User('123', 'Akash')
+user_2 = User('121', 'Angela')
+# This will create object using a constructor function
+# If we don't pass any value while creating object, it will give error.
