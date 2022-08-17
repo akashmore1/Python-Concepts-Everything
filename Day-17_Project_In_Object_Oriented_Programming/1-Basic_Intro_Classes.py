038233f407de250d69745d90384dc964af39345d
Akash More <akashkmore1@gmail.com>
2022-08-17 19:44:31 +0530

    Classes intduction

---
+++ Day-17_Project_In_Object_Oriented_Programming/1-Basic_Intro_Classes.py
@@ -0,0 +1,8 @@
+class User:
+    # Very imp python doesn't like when we don't give indented content in front of ":".
+    pass
+    # So in such cases we can use pass
+    # In name of the class every first letter of every word should be capital i.e. (PascalCase)
+
+
+user_1 = User()
